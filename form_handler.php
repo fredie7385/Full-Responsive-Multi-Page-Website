d6db8ca8af64bd210840eaef598b2eda9ebacaa5
fredie7385 <50455667+fredie7385@users.noreply.github.com>
2023-10-02 16:10:06 +0300

    complete project. added php file to preceive data from contact page to email.

---
+++ form_handler.php
@@ -0,0 +1,22 @@
+<?
+$name =$_POST['name'];
+$visitor_email =$_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'mukii7385@gmail.com';
+$email_subject = 'New Form submission';
+ 
+$email_body =   "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message .\n";
+
+$to = '001fred7385@gmail.com';
+$headers ="From: $email_from\r\n";
+$headers .= "Reply-To $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
